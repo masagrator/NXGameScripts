134eaa964729f939100fa9259da6f0035f1b3244
MasaGratoR <masagrator@gmail.com>
2021-01-01 11:41:09 +0100

    Add files via upload

---
+++ Bioshock/pack.py
@@ -0,0 +1,22 @@
+import numpy
+import platform
+
+with open("dir.txt", 'r') as f:
+    if (platform.system() == "Linux"):
+        filenames = [line.replace("\r\n","\n").split("\n", -1)[0].strip("\n") for line in f]
+    elif (platform.system() == "Windows"): 
+        filenames = [line.split("\r\n", -1)[0].strip("\r\n") for line in f]
+    f.close()
+
+output = open("output.lbf", "wb")
+
+for i in range(0, 99):
+    size1 = len(filenames[i])
+    text = open("output/%s" % (filenames[i]), 'r', encoding='utf-16', newline='')
+    buffer = text.read()
+    text.close()
+    output.write(numpy.uint8(size1+1))
+    output.write(filenames[i].encode('utf-16')[2:])
+    output.write(numpy.uint16(0x0))
+    output.write(numpy.uint32(len(buffer.encode('utf-16'))))
+    output.write(buffer.encode('utf-16'))
---
+++ Bioshock/unpack.py
@@ -0,0 +1,19 @@
+import numpy
+import os
+
+if (os.path.isdir("output") == False):
+    os.makedirs("output")
+
+text = open("Localizedint.lbf", "rb")
+
+while (True):
+    size = numpy.fromfile(text, dtype=numpy.uint8, count=1)
+    buffer = text.read((size[0] * 2)-2)
+    output = open("output/%s" % (buffer.decode('utf-16')), "wb")
+    offset = text.tell() + 2
+    text.seek(offset, 0)
+    size2 = numpy.fromfile(text, dtype=numpy.uint32, count=1)
+    output.write(text.read(size2[0]))
+    output.close()
+
+text.close()
