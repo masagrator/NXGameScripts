254b98876f9f8e138c5d6879a8510d91bd937ca5
MasaGratoR <masagrator@gmail.com>
2021-06-30 19:22:43 +0200

    Add files via upload

---
+++ DESIRE/DESIRE_LZSSDecompressor/DESIRE_LZSSDecompressor.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+size_t LZSS_Decode(void* a1, void* a2)
+
+{
+    unsigned int v2; // w20
+    int v3; // w22
+    int v4; // w24
+    uint8_t* v5; // x19
+    int v6; // w25
+    int v7; // w23
+    char* v8; // x26
+    __int64 v9; // x8
+    int v10; // w9
+    int v11; // w9
+    signed __int64 v12; // x25
+    int v13; // w8
+    int v14; // w21
+
+    v2 = *(uint32_t*)a1;
+    if (a2)
+    {
+        v3 = *((uint32_t*)a1 + 1) - 12;
+        if (v3 >= 1)
+        {
+            v4 = *((uint32_t*)a1 + 2);
+            v5 = (uint8_t*)a2;
+            v6 = 0;
+            v7 = 0;
+            v8 = (char*)a1 + 12;
+            while (1)
+            {
+                v9 = v6;
+                v10 = (unsigned __int8)v8[v6];
+                if (v4 != v10)
+                    break;
+                ++v6;
+                v11 = (unsigned __int8)v8[v9 + 1];
+                if (v4 == v11)
+                {
+                    v5[v7] = v4;
+                LABEL_8:
+                    ++v6;
+                    ++v7;
+                    if (v6 >= v3)
+                        return v2;
+                }
+                else
+                {
+                    v12 = v9 + 2;
+                    v13 = v7 - v11;
+                    v14 = (unsigned __int8)v8[v12];
+                    if (v4 < v11)
+                        ++v13;
+                    memcpy(&v5[v7], &v5[v13], (unsigned __int8)v8[v12]);
+                    v6 = v12 + 1;
+                    v7 += v14;
+                    if (v6 >= v3)
+                        return v2;
+                }
+            }
+            v5[v7] = v10;
+            goto LABEL_8;
+        }
+    }
+    return v2;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    FILE* file = new FILE;
+    std::string var1 = argv[1];
+    std::string var2 = argv[2];
+    fopen_s(&file, var1.c_str(), "rb");
+    uint32_t uncompressed_size = 0;
+    uint32_t compressed_size = 0;
+    fread(&uncompressed_size, 1, 4, file);
+    fread(&compressed_size, 1, 4, file);
+    void* data = (void*)malloc(compressed_size);
+    fseek(file, 0, 0);
+    fread(data, 1, compressed_size, file);
+    void* uncompressed = (void*)malloc(uncompressed_size);
+    size_t size = LZSS_Decode(data, uncompressed);
+    FILE* new_file = new FILE;
+    fopen_s(&new_file, var2.c_str(), "wb");
+    fwrite(uncompressed, size, 1, new_file);
+    std::cout << "Hello World!\n";
+}
