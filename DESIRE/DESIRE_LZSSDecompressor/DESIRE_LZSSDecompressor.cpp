7f3bedc05ecf72b8a34f0369c161b1178fda7060
MasaGratoR <masagrator@gmail.com>
2021-06-30 20:18:20 +0200

    Update DESIRE_LZSSDecompressor.cpp

--- DESIRE/DESIRE_LZSSDecompressor/DESIRE_LZSSDecompressor.cpp
+++ DESIRE/DESIRE_LZSSDecompressor/DESIRE_LZSSDecompressor.cpp
@@ -1,16 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdio.h>
 
-size_t LZSS_Decode(void* a1, void* a2)
 
+size_t LZSS_Decode(void* _src, void* _dst)
 {
-    unsigned int v2; // w20
-    int v3; // w22
-    int v4; // w24
+    unsigned int uncompressed_size; // w20
+    int compressed_size; // w22
+    int compressed_flag; // w24
     uint8_t* v5; // x19
     int v6; // w25
     int v7; // w23
-    char* v8; // x26
+    char* data_pointer; // x26
     __int64 v9; // x8
     int v10; // w9
     int v11; // w9
@@ -18,53 +18,53 @@ size_t LZSS_Decode(void* a1, void* a2)
     int v13; // w8
     int v14; // w21
 
-    v2 = *(uint32_t*)a1;
-    if (a2)
+    uncompressed_size = *(uint32_t*)_src;
+    if (_dst)
     {
-        v3 = *((uint32_t*)a1 + 1) - 12;
-        if (v3 >= 1)
+        compressed_size = *((uint32_t*)_src + 1) - 12;
+        if (compressed_size >= 1)
         {
-            v4 = *((uint32_t*)a1 + 2);
-            v5 = (uint8_t*)a2;
+            compressed_flag = *((uint32_t*)_src + 2);
+            v5 = (uint8_t*)_dst;
             v6 = 0;
             v7 = 0;
-            v8 = (char*)a1 + 12;
+            data_pointer = (char*)_src + 12;
             while (1)
             {
                 v9 = v6;
-                v10 = (unsigned __int8)v8[v6];
-                if (v4 != v10)
+                v10 = (unsigned __int8)data_pointer[v6];
+                if (compressed_flag != v10)
                     break;
                 ++v6;
-                v11 = (unsigned __int8)v8[v9 + 1];
-                if (v4 == v11)
+                v11 = (unsigned __int8)data_pointer[v9 + 1];
+                if (compressed_flag == v11)
                 {
-                    v5[v7] = v4;
+                    v5[v7] = compressed_flag;
                 LABEL_8:
                     ++v6;
                     ++v7;
-                    if (v6 >= v3)
-                        return v2;
+                    if (v6 >= compressed_size)
+                        return uncompressed_size;
                 }
                 else
                 {
                     v12 = v9 + 2;
                     v13 = v7 - v11;
-                    v14 = (unsigned __int8)v8[v12];
-                    if (v4 < v11)
+                    v14 = (unsigned __int8)data_pointer[v12];
+                    if (compressed_flag < v11)
                         ++v13;
-                    memcpy(&v5[v7], &v5[v13], (unsigned __int8)v8[v12]);
+                    memcpy(&v5[v7], &v5[v13], (unsigned __int8)data_pointer[v12]);
                     v6 = v12 + 1;
                     v7 += v14;
-                    if (v6 >= v3)
-                        return v2;
+                    if (v6 >= compressed_size)
+                        return uncompressed_size;
                 }
             }
             v5[v7] = v10;
             goto LABEL_8;
         }
     }
-    return v2;
+    return uncompressed_size;
 }
 
 
