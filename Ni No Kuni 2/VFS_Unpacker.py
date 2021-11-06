a056b516571c44ba493e4da62084292cf19c196d
MasaGratoR <masagrator@gmail.com>
2021-11-06 09:28:05 +0100

    Add files via upload

--- Ni No Kuni 2/VFS_Unpacker.py
+++ Ni No Kuni 2/VFS_Unpacker.py
@@ -19,8 +19,8 @@ def MakeHeader(chunk_size, iterator, uncompressed):
 	buffer_temp = []
 	if (iterator == 0):
 		buffer_temp.append(ZSTD_MAGIC_header)
-		buffer_temp.append(0x0.to_bytes(1, byteorder="little"))
-		buffer_temp.append(0x88.to_bytes(1, byteorder="little"))
+		buffer_temp.append(0x0.to_bytes(1, byteorder="little")) # Set that we don't use any additional fields for frame header
+		buffer_temp.append(0x88.to_bytes(1, byteorder="little")) # Set window size to 128MB which is enough to extract any data from Ni no Kuni 2
 	compressed_block_flag = "100"
 	uncompressed_block_flag = "000"
 	bin_c_size = bin(chunk_size)[2:]
@@ -28,7 +28,7 @@ def MakeHeader(chunk_size, iterator, uncompressed):
 		bin_compress_size = bin_c_size + uncompressed_block_flag
 	else:
 		bin_compress_size = bin_c_size + compressed_block_flag
-	com_size = int(bin_compress_size, base=2).to_bytes(3, byteorder="little")
+	com_size = int(bin_compress_size, base=2).to_bytes(3, byteorder="little") # Set size of chunk with information if it's compressed
 	buffer_temp.append(com_size)
 	return b"".join(buffer_temp)
 
@@ -44,7 +44,7 @@ def Decompress(data, u_size, chunks, dec_dict_ID, dec_dicts):
 	for i in range(0, len(chunks)):
 		uncompressed = False
 		if (chunks[i] < 0):
-			size = chunks[i] * -1
+			size = chunks[i] * -1 # Uncompressed size is originally stored as negative value
 			uncompressed = True
 		else:
 			size = chunks[i]
@@ -54,10 +54,10 @@ def Decompress(data, u_size, chunks, dec_dict_ID, dec_dicts):
 		temp_list.append(data_temp)
 		list.append(b"".join(temp_list))
 		sumarum += size
-	list.append(b"\x01\x00\x00")
+	list.append(b"\x01\x00\x00") # ZSTD EOF
 	return dctx.decompress(b"".join(list), max_output_size=u_size)
 
-file = open("preload_zstd.vfs", "rb")
+file = open(sys.argv[1], "rb")
 
 #Read header data
 if (file.read(0x4) != b"VFS3"):
@@ -89,7 +89,8 @@ Temp = numpy.fromfile(file, dtype=numpy.uint64, count=3)
 Chunk_dictionary_offset = Temp[0]
 Decompression_dictionary_offset = Temp[1]
 StringListOffset = Temp[2]
-file.seek(4, 1)
+while (file.tell() % 16 != 0):
+	file.seek(1, 1)
 
 #Offsets in file dictionary are relative to this position, so we need to save offset.
 OldPlace = file.tell()
@@ -151,9 +152,9 @@ for i in range(0, file_count):
 	entry["CRC"] = file.read(0x4).hex()
 	Temp = numpy.fromfile(file, dtype=numpy.uint32, count=2)
 	entry["FolderID"] = Temp[1]
-	entry["COMPRESSION"] = numpy.fromfile(file, dtype=numpy.int16, count=1)[0] #Compression method
-	if (entry["COMPRESSION"] != 3):
-		print("UNKNOWN FLAG! %d" % entry["COMPRESSION"])
+	entry["COMPRESSION"] = numpy.fromfile(file, dtype=numpy.int16, count=1)[0]
+	if (entry["COMPRESSION"] != 3): # VFS supports other compressions. In Ni no Kuni 2 all files are compressed with zstd which matches "3"
+		print("UNKNOWN COMPRESSION! %d" % entry["COMPRESSION"])
 		sys.exit()
 	entry["DEC_DICT"] = numpy.fromfile(file, dtype=numpy.int16, count=1)[0]
 	entry["FULLPATH"] = "%s/%s" % (Folders["0x%x" % Temp[1]], File_names[Temp[0]])
@@ -167,17 +168,17 @@ file.seek(Chunk_dictionary_offset, 0)
 ChunkTableSize = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0]
 while (file.tell() < (Chunk_dictionary_offset + ChunkTableSize)):
 	entry = {}
-	size = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0]
+	size = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0] # Size of chunk size table which is equal to: count of entries * size of int32
 	entry["SIZES"] = numpy.fromfile(file, dtype=numpy.int32, count=int(size / 4))
 	Chunks.append(entry)
 
 #Read Decompression Dicts dictionary
 decompress_dictionary_count = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0]
-size_dec_dict = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0]
+size_dec_dict = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0] # Size of Decompression Dicts dictionary
 
 Dec_dicts = []
 for i in range(0, decompress_dictionary_count):
-	dict_size = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0]
+	dict_size = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0] # Size of one Decompression Dict
 	Dec_dicts.append(file.read(dict_size))
 
 print("Indexed %d compression dictionaries." % len(Dec_dicts))
@@ -185,24 +186,28 @@ print("Indexed %d compression dictionaries." % len(Dec_dicts))
 Files_SUCCESS = 0
 Files_FAIL = 0
 
+UNC_SIZE_WHOLE = 0
+
+for i in range(0, len(Files)):
+	UNC_SIZE_WHOLE += Files["0x%x" % i]["U_SIZE"]
+
+print("Unpacking requires %d B / %0.2f MB of free space. To continue press ENTER" % (UNC_SIZE_WHOLE, UNC_SIZE_WHOLE / 1024 / 1024))
+input()
+
 print("Decompression in work...")
 # Write Data
 for i in range(0, len(Files)):
 	Offset = OldPlace + int(Files["0x%x" % i]["OFFSET"])
 	file.seek(Offset, 0)
-	if ((Files["0x%x" % i]["C_SIZE"] == 0) or (Files["0x%x" % i]["C_SIZE"] == Files["0x%x" % i]["U_SIZE"])):
-		data.append(file.read(Files["0x%x" % i]["U_SIZE"]))
+	if (Files["0x%x" % i]["C_SIZE"] == Files["0x%x" % i]["U_SIZE"]):
+		print("UNCOMPRESSED: %s, c_size: %d, u_size: %d" % (Files["0x%x" % i]["FULLPATH"], Files["0x%x" % i]["C_SIZE"], Files["0x%x" % i]["U_SIZE"]))
+		data = file.read(Files["0x%x" % i]["U_SIZE"])
 	else:
 		buffer_temp = (file.read(Files["0x%x" % i]["C_SIZE"]))
 		try:
 			data = Decompress(buffer_temp, Files["0x%x" % i]["U_SIZE"], Chunks[i]["SIZES"], Files["0x%x" % i]["DEC_DICT"], Dec_dicts)
 		except Exception as Exception_handle:
 			DebugOutput("%s, %s, chunks_count: %d, dec_dict_id=%d, u_size: %d" % (Files["0x%x" % i]["FULLPATH"], Exception_handle, len(Chunks[i]["SIZES"]), Files["0x%x" % i]["DEC_DICT"], Files["0x%x" % i]["U_SIZE"]))
-			DebugOutput(" ".join(str(e) for e in Chunks[i]["SIZES"]))
-			size = Files["0x%x" % i]["U_SIZE"]
-			for i in range(0, len(Chunks[i]["SIZES"])):
-				DebugOutput("%d" % (size))
-				size -= 131072
 			Files_FAIL += 1
 			continue
 
