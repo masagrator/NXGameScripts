bf8f4508aad4a3d09e205dad5e8716617e05bb00
MasaGratoR <masagrator@gmail.com>
2022-02-13 11:59:44 +0100

    Add files via upload

--- Cold Steel 1/phyrepacker.py
+++ Cold Steel 1/phyrepacker.py
@@ -3,8 +3,12 @@ import glob
 import os
 import sys
 
-files = glob.glob("UNPACKED/*/*.dds.header")
+def SortElem(elem):
+    return elem
+
 files = glob.glob("UNPACKED/*/*.png.header")
+files += glob.glob("UNPACKED/*/*.dds.header")
+files.sort(key=SortElem)
 
 os.makedirs("REPACKED", exist_ok=True)
 
@@ -12,14 +16,21 @@ for i in range(0, len(files)):
     print(files[i][9:])
     file = open(files[i], "rb")
     file.seek(0, 2)
-    file.seek((0x42) * -1, 1)
+    if (files[i][-10:-7] == "png"):
+        file.seek((0x43) * -1, 1)
+    else:
+        file.seek((0x42) * -1, 1)
     width = int.from_bytes(file.read(4), byteorder="little")
     height = int.from_bytes(file.read(4), byteorder="little")
     file.seek(0, 2)
-    file.seek((0x4A) * -1, 1)
+    if (files[i][-10:-7] == "png"):
+        file.seek((0x4C) * -1, 1)
+    else:
+        file.seek((0x4B) * -1, 1)
     mipmaps = int.from_bytes(file.read(1), byteorder="little")
-    file.seek(0x1F, 1)
+    file.seek(0x20, 1)
     type = file.read(4).decode("ASCII")
+    if (type == "RGBA"): type = "no"
     file.seek(0, 0)
     header = file.read()
     file.close()
@@ -47,8 +58,16 @@ for i in range(0, len(files)):
     os.makedirs("REPACKED/%s" % os.path.dirname(files[i][9:]), exist_ok=True)
     file = open("REPACKED/%s.phyre" % files[i][9:-7], "wb")
     file.write(header)
-    if (type == "RGBA"):
-        file.write(blob[0x94:])
+    blob = blob[0x80:]
+    if (type == "no"):
+        new_blob = []
+        print("Repacking from BGRA to RGBA... Please wait a while.")
+        for x in range(0, int(len(blob) / 4)):
+            new_blob.append(blob[2+(4*x):3+(4*x)])
+            new_blob.append(blob[1+(4*x):2+(4*x)])
+            new_blob.append(blob[0+(4*x):1+(4*x)])
+            new_blob.append(blob[3+(4*x):4+(4*x)])
+        file.write(b"".join(new_blob))
     else:
-        file.write(blob[0x80:])
+        file.write(blob)
     file.close()
