75998ea90189c2438910948078b5fd5bd8b9ce47
MasaGratoR <masagrator@gmail.com>
2022-07-14 17:02:58 +0200

    Add files via upload

--- Summer Pockets REFLECTION BLUE/image_repacker.py
+++ Summer Pockets REFLECTION BLUE/image_repacker.py
@@ -22,13 +22,14 @@ flag2 = numpy.fromfile(file, dtype=numpy.int8, count=1)[0]
 flag3 = numpy.fromfile(file, dtype=numpy.int8, count=1)[0]
 offset_start_file_names = numpy.fromfile(file, dtype=numpy.uint32, count=1)[0]
 
-if (flag0 != 0):
+if (flag1 not in [0, 2, 3]):
     print("UNSUPPORTED PAK TYPE")
     sys.exit()
 
-if (flag1 not in [2, 3]):
-    print("UNSUPPORTED PAK TYPE")
-    sys.exit()
+if (flag1 == 0):
+	if (flag0 not in [0, 1]):
+		print("UNSUPPORTED PAK TYPE")
+		sys.exit()
 
 file_table = {}
 file_table['offset'] = []
@@ -40,13 +41,14 @@ for i in range(0, file_count):
 
 if (flag1 == 2):
 	assert(file.tell() == offset_start_file_names)
-else:
+elif (flag0 == 0):
 	file.seek(offset_start_file_names)
 
-filename_table = []
+if (flag0 != 1):
+	filename_table = []
 
-for i in range(0, file_count):
-	filename_table.append(readString(file))
+	for i in range(0, file_count):
+		filename_table.append(readString(file))
 
 if (len(sys.argv) == 3):
 	new_file = open("%s" % (sys.argv[2]), "wb")
@@ -55,19 +57,28 @@ else:
 file.seek(0)
 new_file.write(file.read(header_size))
 file.seek(0)
-new_file.seek(0x28)
+if (flag0 == 0):
+	new_file.seek(0x28)
+else:
+	new_file.seek(0x2C)
 
 scripts_size = []
 
 for i in range(0, file_count):
 	if (file_table["offset"][i] == 0):
 		try:
-			script = open("%s\%s.dat" % (sys.argv[1][:-4], filename_table[i]), "rb")
+			if (flag0 == 0):
+				script = open("%s\%s.dat" % (sys.argv[1][:-4], filename_table[i]), "rb")
+			else:
+				script = open("%s\%04d.dat" % (sys.argv[1][:-4], i), "rb")
 		except:
 			scripts_size.append(0)
 			continue
 	else:
-		script = open("%s\%s.dat" % (sys.argv[1][:-4], filename_table[i]), "rb")
+		if (flag0 == 0):
+			script = open("%s\%s.dat" % (sys.argv[1][:-4], filename_table[i]), "rb")
+		else:
+			script = open("%s\%04d.dat" % (sys.argv[1][:-4], i), "rb")
 	script.seek(0, 2)
 	scripts_size.append(script.tell())
 	script.close()
@@ -82,11 +93,14 @@ for i in range(0, file_count):
 	new_file.write(numpy.uint32(scripts_size[i]))
 	offset += int(round((scripts_size[i]+(round_to/2-1)) / round_to))
 
-new_file.seek(offset_start_file_names)
+if (flag0 == 0):
+	new_file.seek(offset_start_file_names)
 
-for i in range(0, file_count):
-	new_file.write(filename_table[i].encode("shift_jis_2004"))
-	new_file.write(b"\x00")
+	for i in range(0, file_count):
+		new_file.write(filename_table[i].encode("shift_jis_2004"))
+		new_file.write(b"\x00")
+else:
+	new_file.seek(0, 2)
 
 file.seek(new_file.tell(), 0)
 while(True):
@@ -96,7 +110,10 @@ while(True):
 for i in range(0, file_count):
 	if (scripts_size[i] == 0):
 		continue
-	script = open("%s\%s.dat" % (sys.argv[1][:-4], filename_table[i]), "rb")
+	if (flag0 == 0):
+		script = open("%s\%s.dat" % (sys.argv[1][:-4], filename_table[i]), "rb")
+	else:
+		script = open("%s\%04d.dat" % (sys.argv[1][:-4], i), "rb")
 	temp = script.read()
 	new_file.write(temp)
 	if (len(temp) != (round_to * round((scripts_size[i]+(round_to/2-1)) / round_to))):
