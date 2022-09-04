04fa3e1325052ac0030ddb17bd760a34ef614929
MasaGratoR <masagrator@gmail.com>
2022-09-04 18:51:45 +0200

    Update and rename extgzToPNG.py to extToPNG.py

--- 9-nine/extToPNG.py
+++ 9-nine/extToPNG.py
@@ -2,8 +2,12 @@ import gzip
 import sys
 from PIL import Image
 from pathlib import Path
+import numpy
 
-file = gzip.open(sys.argv[1])
+if (Path(sys.argv[1]).suffix == ".gz"):
+    file = gzip.open(sys.argv[1])
+else:
+    file = open(sys.argv[1], "rb")
 
 if (file.read(4) != b"EXT0"):
     print("WRONG MAGIC!")
@@ -19,11 +23,18 @@ texture_height = int.from_bytes(file.read(4), "little", signed=False)
 file.seek(8, 1)
 
 BPP = int.from_bytes(file.read(4), "little", signed=False)
-if (BPP != 32):
+if (BPP not in [32, 8]):
     print("Unsupported BPP: %d" % BPP)
     sys.exit()
 
 file.seek(0x100)
 
-img = Image.frombuffer("RGBA", (real_texture_width, real_texture_height), file.read(), "raw", "BGRA", 0, 1)
+if (BPP == 32):
+    img = Image.frombuffer("RGBA", (real_texture_width, real_texture_height), file.read(), "raw", "BGRA", 0, 1)
+else:
+    pallette = numpy.fromfile(file, dtype=numpy.uint32, count=256)
+    buffer = []
+    for i in range(real_texture_width * real_texture_height):
+        buffer.append(pallette[file.read(1)[0]])
+    img = Image.frombuffer("RGBA", (real_texture_width, real_texture_height), b"".join(buffer), "raw", "BGRA", 0, 1)
 img.save("%s.png" % Path(sys.argv[1]).stem)
