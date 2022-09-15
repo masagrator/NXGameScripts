09fe1f63118ef01c319838d7f004250541b49f82
MasaGratoR <masagrator@gmail.com>
2022-09-15 22:49:54 +0200

    Update NXAEnc.c

--- Sakuna Of Rice and Ruin/OpusEncoder/NXAEnc.c
+++ Sakuna Of Rice and Ruin/OpusEncoder/NXAEnc.c
@@ -1,10 +1,7 @@
 /*Work based on code from here:
 https://gist.github.com/tellowkrinkle/91423d561d8976be418ba770b9499bb3
 
-Don't touch CLI settings, just put wav file as input and define output.
-WAV File was converted correctly only if file was:
-- 16-bit
-- Mono
+PCM File was converted correctly only if file was:
 - 48000 Hz
 - PCM S16LE
 */
