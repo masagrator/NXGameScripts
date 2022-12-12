7893cc06d014a4eef08bc959113459970afcec8d
MasaGratoR <masagrator@gmail.com>
2022-12-12 23:06:45 +0100

    Add files via upload

--- Crisis Core Final Fantasy VII Reunion/pkg_unpacker.py
+++ Crisis Core Final Fantasy VII Reunion/pkg_unpacker.py
@@ -19,11 +19,12 @@ entry_count = int.from_bytes(file.read(8), "little")
 DATA = []
 
 for i in range(entry_count):
-    isEntry = int.from_bytes(file.read(4), "little")
-    if (isEntry == 0):
+    unc_buffer_size = int.from_bytes(file.read(4), "little")
+    if (unc_buffer_size == 0):
         file.seek(12,1)
         continue
     entry = {}
+    entry["unc_buffer_size"] = unc_buffer_size
     entry["ID"] = i
     entry["unc_size"] = int.from_bytes(file.read(4), "little")
     entry["offset"] = int.from_bytes(file.read(8), "little")
@@ -37,12 +38,12 @@ for i in range(filtered_entry_count):
     print("Unpacking file: %d/%d" % (i+1, filtered_entry_count), end="\r")
     file.seek(DATA[i]["offset"])
     chunks = []
-    for x in range(math.ceil(DATA[i]["unc_size"] / 0x8000)):
+    for x in range(math.ceil(DATA[i]["unc_size"] / DATA[i]["unc_buffer_size"])):
         chunks.append(int.from_bytes(file.read(4), "little"))
     chunks_count = len(chunks)
     new_file = open("unpacked/%05d.dat" % DATA[i]["ID"], "wb")
     for x in range(chunks_count):
         buffer = file.read(chunks[x])
-        unc_buffer = zlib.decompress(buffer, bufsize=0x8000)
+        unc_buffer = zlib.decompress(buffer, bufsize=DATA[i]["unc_buffer_size"])
         new_file.write(unc_buffer)
     new_file.close()
