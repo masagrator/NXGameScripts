b206cb7a5457ea1d7a8dc3f04c29435254b7bb84
MasaGratoR <masagrator@gmail.com>
2022-12-26 22:44:57 +0100

    Add files via upload

--- Crisis Core Final Fantasy VII Reunion/pkg_packer.py
+++ Crisis Core Final Fantasy VII Reunion/pkg_packer.py
@@ -23,6 +23,7 @@ def printUsage():
 	print("-e: entry count <number>, default: 0x7118")
 	print("-m: max chunk size <number>, default: 0x8000")
 	print("-l: compression level <number 0-9>, default: 2")
+	print("-h: this lowers IO lag at the cost of higher RAM usage (1 GB of free RAM expected)")
 	sys.exit()
 
 repack = False
@@ -32,6 +33,7 @@ chunk_size = 0x8000
 entry_size = 0x10
 header_size = 0x10
 compression_level = 2
+highRAM = False
 
 if (len(sys.argv) < 2):
 	printUsage()
@@ -55,6 +57,9 @@ if (len(sys.argv) > 2):
 			case "-l":
 				compression_level = int(sys.argv[i+1])
 				i += 2
+			case "-h":
+				highRAM = True
+				i += 1
 			case _:
 				printUsage()
 
@@ -87,7 +92,9 @@ if repack == False:
 			x += 1
 			table.append(b"\x00" * 8)
 		else:
-			table.append(b"\x00" * entry_size)
+			table.append(b"\x00" * 4)
+			table.append(b"\x00" * 4)
+			table.append(b"\x00" * 8)
 	print("Finished calculating %d files" % file_count)
 
 	size_now = len(b"".join(header) + b"".join(table))
@@ -97,30 +104,54 @@ if repack == False:
 		print("Got: 0x%x" % size_now)
 		sys.exit()
 	
-	new_file = open(output, "wb")
-	new_file.write(b"".join(header))
-	new_file.write(b"".join(table))
-	
 	offset = data_blob_offset
 
-	for i in range(file_count):
-		print("Compressing file %d/%d" % (i+1, file_count), end="\r")
-		filesize = os.stat(files[i]).st_size
-		temp_file = open(files[i], 'rb')
-		compressed_data = []
-		while(temp_file.tell() < filesize):
-			compressed_data.append(zlib.compress(temp_file.read(chunk_size), level=compression_level))
-		temp_file.close()
-		for x in range(len(compressed_data)):
-			new_file.write(len(compressed_data[x]).to_bytes(4, "little"))
-		for x in range(len(compressed_data)):
-			new_file.write(compressed_data[x])
-		new_file.seek(header_size + (int(Path(files[i]).stem, base=10) * entry_size) + 0x8)
-		new_file.write(offset.to_bytes(8, "little"))
-		new_file.seek(0, 2)
-		offset = new_file.tell()
-	new_file.write(b"\x00" * 7)
-	new_file.close()
+	if (highRAM == True): 
+		BLOB = []
+		for i in range(file_count):
+			print("Compressing file %d/%d" % (i+1, file_count), end="\r")
+			filesize = os.stat(files[i]).st_size
+			temp_file = open(files[i], 'rb')
+			compressed_data = []
+			while(temp_file.tell() < filesize):
+				compressed_data.append(zlib.compress(temp_file.read(chunk_size), level=compression_level))
+			temp_file.close()
+			temp_size = 0
+			for x in range(len(compressed_data)):
+				chunk_temp_size = len(compressed_data[x])
+				BLOB.append(chunk_temp_size.to_bytes(4, "little"))
+				temp_size += chunk_temp_size + 4
+			for x in range(len(compressed_data)):
+				BLOB.append(compressed_data[x])
+			table[int(Path(files[i]).stem, base=10) * 3 + 2] = offset.to_bytes(8, "little")
+			offset += temp_size
+		new_file = open(output, "wb")
+		new_file.write(b"".join(header))
+		new_file.write(b"".join(table))
+		new_file.write(b"".join(BLOB))
+		new_file.close()
+	else:
+		new_file = open(output, "wb")
+		new_file.write(b"".join(header))
+		new_file.write(b"".join(table))
+		for i in range(file_count):
+			print("Compressing file %d/%d" % (i+1, file_count), end="\r")
+			filesize = os.stat(files[i]).st_size
+			temp_file = open(files[i], 'rb')
+			compressed_data = []
+			while(temp_file.tell() < filesize):
+				compressed_data.append(zlib.compress(temp_file.read(chunk_size), level=compression_level))
+			temp_file.close()
+			for x in range(len(compressed_data)):
+				new_file.write(len(compressed_data[x]).to_bytes(4, "little"))
+			for x in range(len(compressed_data)):
+				new_file.write(compressed_data[x])
+			new_file.seek(header_size + (int(Path(files[i]).stem, base=10) * entry_size) + 0x8)
+			new_file.write(offset.to_bytes(8, "little"))
+			new_file.seek(0, 2)
+			offset = new_file.tell()
+		new_file.close()
+
 	print("Finished executing script!")
 else:
 	print("Repack not implemented")
