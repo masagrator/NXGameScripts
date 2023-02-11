18169c683a1a5be00c9b1ee53f5f0b706cde4bfe
MasaGratoR <masagrator@gmail.com>
2023-02-11 11:02:53 +0100

    Add files via upload

--- Aonatsu Line/Script_assembler_re.py
+++ Aonatsu Line/Script_assembler_re.py
@@ -209,7 +209,7 @@ for i in range(0, len(files)):
 				case "LOAD_STRING":
 					DUMP.append(0x0.to_bytes(4, "little"))
 					try:
-						Args[2].encode("ISO-8859-1")
+						Args[2].encode("ISO-8859-15")
 					except:
 						pass
 					else:
