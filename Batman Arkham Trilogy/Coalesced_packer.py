fc96ad10a364ef4885afe780fd6b2c10a4bd329c
MasaGratoR <masagrator@gmail.com>
2023-12-01 20:31:26 +0100

    Add files via upload

---
+++ Batman Arkham Trilogy/Coalesced_packer.py
@@ -0,0 +1,83 @@
+# Provide folder created with Coalesced_unpacker.py as argument, it will create a .bin file with the same name in "new" folder
+# example: python Coalesced_packer.py COALESCED_INT
+
+import sys
+import os
+import json
+
+with open(f"{sys.argv[1]}/fileorder.json", "r", encoding="UTF-8") as file:
+	files = json.load(file)
+
+DUMP = [len(files).to_bytes(4, "little")]
+
+for i in range(len(files)):
+	null_terminator = 1
+	try:
+		string = files[i].encode("iso-8859-1")
+	except:
+		string = files[i].encode("UTF-16-LE")
+		null_terminator = -2
+	string_len = int(len(string) / null_terminator) + 1
+	DUMP.append(string_len.to_bytes(4, "little", signed=True))
+	DUMP.append(string)
+	DUMP.append(b"\x00" * abs(null_terminator))
+	file = open("%s/%s.json" % (sys.argv[1], files[i][3:]), "r", encoding="UTF-8")
+	config = json.load(file)
+	file.close()
+	DUMP.append(len(config.keys()).to_bytes(4, "little"))
+	sections = list(config.keys())
+	for x in range(len(sections)):
+		null_terminator = 1
+		try:
+			string = sections[x].encode("iso-8859-1")
+		except:
+			string = sections[x].encode("UTF-16-LE")
+			null_terminator = -2
+		string_len = 1
+		for i in range(0, len(string), abs(null_terminator)):
+			string_len += 1
+		if (null_terminator == -2):
+			string_len = ~string_len + 1
+		DUMP.append(string_len.to_bytes(4, "little", signed=True))
+		DUMP.append(string)
+		DUMP.append(b"\x00" * abs(null_terminator))
+		pairs_count = len(config[sections[x]])
+		DUMP.append(pairs_count.to_bytes(4, "little"))	
+		for entry in config[sections[x]]:
+			null_terminator = 1
+			try:
+				string = entry["key"].encode("iso-8859-1")
+			except:
+				string = entry["key"].encode("UTF-16-LE")
+				null_terminator = -2
+			key_len = 1
+			for i in range(0, len(string), abs(null_terminator)):
+				key_len += 1
+			if (null_terminator == -2):
+				key_len = ~key_len + 1
+			DUMP.append(key_len.to_bytes(4, "little", signed=True))
+			DUMP.append(string)
+			DUMP.append(b"\x00" * abs(null_terminator))
+			null_terminator = 1
+			try:
+				string = entry["value"].encode("iso-8859-1")
+			except:
+				string = entry["value"].encode("UTF-16-LE")
+				null_terminator = -2
+			value_len = 1
+			for i in range(0, len(string), abs(null_terminator)):
+				value_len += 1	
+			if (value_len == 1):
+				value_len = 0
+				null_terminator = 0
+			if (null_terminator == -2):
+				value_len = ~value_len + 1
+			DUMP.append(value_len.to_bytes(4, "little", signed=True))
+			DUMP.append(string)
+			DUMP.append(b"\x00" * abs(null_terminator))			
+
+
+os.makedirs("new", exist_ok=True)
+new_file = open("new/%s.bin" % (sys.argv[1]), "wb")
+new_file.write(b"".join(DUMP))
+new_file.close()
---
+++ Batman Arkham Trilogy/Coalesced_unpacker.py
@@ -0,0 +1,55 @@
+# Provide .bin file as argument, it will create a folder with the same name
+# example: python Coalesced_unpacker.py COALESCED_INT.bin
+
+import sys
+import os
+from pathlib import Path
+import json
+
+file = open(sys.argv[1], "rb")
+
+file_count = int.from_bytes(file.read(4), "little")
+
+filelist = []
+
+for i in range(file_count):
+    path_length = int.from_bytes(file.read(4), "little", signed=True)
+    if (path_length < 0):
+        path_length *= -2
+        path_name = file.read(path_length)[:-2].decode("UTF-16-LE")
+    else:
+        path_name = file.read(path_length)[:-1].decode("iso-8859-1")
+    filelist.append(path_name)
+    path_name = path_name[3:]
+    section_count = int.from_bytes(file.read(4), "little")
+    SECTIONS = {}
+    for x in range(section_count):
+        section_name_length = int.from_bytes(file.read(4), "little", signed=True)
+        if (section_name_length < 0):
+            section_name_length *= -2
+            section_name = file.read(section_name_length)[:-2].decode("UTF-16-LE")
+        else:
+            section_name = file.read(section_name_length)[:-1].decode("iso-8859-1")
+        SECTIONS[section_name] = []
+        key_value_pair_count = int.from_bytes(file.read(4), "little")
+        for y in range(key_value_pair_count):
+            key_name_length = int.from_bytes(file.read(4), "little", signed=True)
+            if (key_name_length < 0):
+                key_name_length *= -2
+                key_name = file.read(key_name_length)[:-2].decode("UTF-16-LE")
+            else:
+                key_name = file.read(key_name_length)[:-1].decode("iso-8859-1")
+            value_length = int.from_bytes(file.read(4), "little", signed=True)
+            if (value_length < 0):
+                value_length *= -2
+                value_string = file.read(value_length)[:-2].decode("UTF-16-LE")
+            else:  
+                value_string = file.read(value_length)[:-1].decode("iso-8859-1")
+            SECTIONS[section_name].append({"key": key_name, "value": value_string})
+    os.makedirs("%s/%s" % (Path(sys.argv[1]).stem, Path(path_name).parent), exist_ok=True)
+
+    with open("%s/%s.json" % (Path(sys.argv[1]).stem, path_name), "w", encoding="UTF-8") as new_file:
+        json.dump(SECTIONS, new_file, indent="\t", ensure_ascii=False)
+
+    with open("%s/fileorder.json" % Path(sys.argv[1]).stem, "w", encoding="UTF-8") as new_file:
+        json.dump(filelist, new_file, indent="\t", ensure_ascii=False)
