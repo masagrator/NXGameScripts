948c51c64f3d1ed82e56b51eb3d476e3ef9dfeee
MasaGratoR <masagrator@gmail.com>
2025-03-24 09:56:30 +0100

    Add files via upload

---
+++ XenobladeChroniclesX/pck_unpack.py
@@ -0,0 +1,83 @@
+import sys
+import os
+from pathlib import Path
+
+file = open(sys.argv[1], "rb")
+if (file.read(4) != b"AKPK"):
+    file.close()
+    print("WRONG MAGIC!")
+    sys.exit()
+header_size = int.from_bytes(file.read(4), "little") # not ideally aligned?
+version = int.from_bytes(file.read(4), "little")
+LangMapsSize = int.from_bytes(file.read(4), "little")
+BanksSize = int.from_bytes(file.read(4), "little")
+StreamsSize = int.from_bytes(file.read(4), "little")
+ExternalSize = int.from_bytes(file.read(4), "little")
+
+if (LangMapsSize != 0):
+    Langs_count = int.from_bytes(file.read(4), "little")
+    if (Langs_count):
+        lang_entry_size = (LangMapsSize - 4) // Langs_count
+        for i in range(Langs_count):
+            file.seek(lang_entry_size, 1)
+
+BANKS = []
+
+if (BanksSize != 0):
+    Banks_count = int.from_bytes(file.read(4), "little")
+    if (Banks_count):
+        for i in range(Banks_count):
+            entry = {}
+            entry["hash"] = int.from_bytes(file.read(4), "little")
+            offset_multiplier = int.from_bytes(file.read(4), "little")
+            entry["size"] = int.from_bytes(file.read(4), "little")
+            entry["offset"] = int.from_bytes(file.read(4), "little") * offset_multiplier
+            assert (int.from_bytes(file.read(4), "little") == 0)
+            BANKS.append(entry)
+
+STREAMS = []
+if (StreamsSize):
+    Streams_count = int.from_bytes(file.read(4), "little")
+    if (Streams_count):
+        for i in range(Streams_count):
+            entry = {}
+            entry["hash"] = int.from_bytes(file.read(4), "little")
+            offset_multiplier = int.from_bytes(file.read(4), "little")
+            entry["size"] = int.from_bytes(file.read(4), "little")
+            entry["offset"] = int.from_bytes(file.read(4), "little") * offset_multiplier
+            assert (int.from_bytes(file.read(4), "little") == 0)
+            STREAMS.append(entry)
+
+
+if (ExternalSize):
+    Externals_count = int.from_bytes(file.read(4), "little")
+    if (Externals_count):
+        External_entry_size = (ExternalSize - 4) // Externals_count
+        for i in range(Externals_count):
+            file.seek(External_entry_size, 1)
+
+print("Banks detected:", len(BANKS))
+print("Streams detected:", len(STREAMS))
+
+os.makedirs("%s/BANKS" % Path(sys.argv[1]).stem, exist_ok=True)
+os.makedirs("%s/STREAMS" % Path(sys.argv[1]).stem, exist_ok=True)
+
+for i in range(len(BANKS)):
+    print("Unpacking bank", i+1)
+    file.seek(BANKS[i]["offset"], 0)
+    new_file = open("%s/BANKS/%08X.bnk" % (Path(sys.argv[1]).stem, BANKS[i]["hash"]), "wb")
+    new_file.write(file.read(BANKS[i]["size"]))
+    new_file.close()
+
+print("Unpacked all banks!")
+
+for i in range(len(STREAMS)):
+    print("Unpacking stream", i+1)
+    file.seek(STREAMS[i]["offset"], 0)
+    new_file = open("%s/STREAMS/%08X.wav" % (Path(sys.argv[1]).stem, STREAMS[i]["hash"]), "wb")
+    new_file.write(file.read(STREAMS[i]["size"]))
+    new_file.close()
+
+print("Unpacked all streams!")
+
+file.close()
